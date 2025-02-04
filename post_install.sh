d810870e753b20e80b097bc8b079b4e2910cdb68
cabf05 <cabf05@gmail.com>
2025-02-04 15:56:09 -0500

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-set -e  # Para parar em caso de erro
+set -e
 
-# Instala dependências do sistema
-sudo apt-get update -qq
-sudo apt-get install -y \
-    tesseract-ocr \
-    tesseract-ocr-por \
-    libleptonica-dev \
-    libtesseract-dev \
-    poppler-utils
-
-# Verifica instalação do Tesseract
-echo "Versão do Tesseract:"
-tesseract --version
-
-# Instala dependências Python
 python -m pip install --upgrade pip
-pip install --no-cache-dir -r requirements.txt
-
-# Download de recursos NLTK
-python -c "import nltk; nltk.download('stopwords'); nltk.download('punkt'); nltk.download('wordnet'); nltk.download('omw-1.4')"
+pip install -r requirements.txt
